c983e62d1788ea2ad53c397ebf40c5425f2e02d6
aniketambore <aniketambore0@gmail.com>
2023-10-22 19:42:37 +0530

    build_runner error fixing

--- lib/env.dart
+++ lib/env.dart
@@ -5,14 +5,14 @@ part 'env.g.dart';
 @Envied(path: '.env')
 abstract class Env {
   @EnviedField(varName: 'btc-pay-token', obfuscate: true)
-  static final btcPayToken = _Env.btcPayToken;
+  static final String btcPayToken = _Env.btcPayToken;
 
   @EnviedField(varName: 'btc-pay-username', obfuscate: true)
-  static final btcPayUsername = _Env.btcPayUsername;
+  static final String btcPayUsername = _Env.btcPayUsername;
 
   @EnviedField(varName: 'btc-pay-password', obfuscate: true)
-  static final btcPayPassword = _Env.btcPayPassword;
+  static final String btcPayPassword = _Env.btcPayPassword;
 
   @EnviedField(varName: 'btc-pay-url', obfuscate: true)
-  static final btcPayUrl = _Env.btcPayUrl;
+  static final String btcPayUrl = _Env.btcPayUrl;
 }
